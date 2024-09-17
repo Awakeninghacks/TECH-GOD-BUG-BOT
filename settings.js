a835178c6f291b3246073563a2b06cd9c735fd7c
Awakeninghacks <166938169+Awakeninghacks@users.noreply.github.com>
2024-09-17 18:38:37 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,8 +16,8 @@ global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
+global.ownernumber = '+27736421417'  //creator number
+global.ownername = 'AwaKeNiNg' //owner name
 global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
